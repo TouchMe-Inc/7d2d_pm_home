8455cb3f8926b95ad2bc871de1fc4d87d6d5c7b4
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2026-01-24 14:24:03 +0300

    Init

---
+++ src/HomePlugin/TeleportPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace HomePlugin;
+
+public class TeleportPoint
+{
+    public string UserId { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+}
